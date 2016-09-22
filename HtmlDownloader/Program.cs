7d371420f6e6006d1a7ec74876072631f6f0fb3d
alunfes <ts4792@gmail.com>
2016-09-22 12:05:21 +0900

    initial commit

---
+++ HtmlDownloader/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+
+namespace HtmlDownloader
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Please input url");
+			string url = Console.ReadLine();
+
+			try
+			{
+				using (WebClient wc = new WebClient())
+				{
+					string html = wc.DownloadString(url);
+					Console.WriteLine(html);
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Unexpected error occurred!");
+				Console.WriteLine(e.ToString());
+			}
+		}
+	}
+}
